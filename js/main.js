6ab83ef003b01b43c3768aef5bb61046af330a3d
Mustafa Emin <mustafaeminbosnali@hotmail.com>
2020-09-07 16:01:40 +0300

    JS Modified

--- js/main.js
+++ js/main.js
@@ -1,63 +1,7 @@
 document.addEventListener('keydown', (event) => {
     const keyName = event.key;
-    const div1 = document.getElementById("box-1");
-    const div2 = document.getElementById("box-2");
-    const div3 = document.getElementById("box-3");
-    const div4 = document.getElementById("box-4");
-    const div5 = document.getElementById("box-5");
-    const div6 = document.getElementById("box-6");
-    const div7 = document.getElementById("box-7");
-    const div8 = document.getElementById("box-8");
-    const div9 = document.getElementById("box-9");
-    const div10 = document.getElementById("box-10");
-    if(keyName === "1"){
-        //Pressed Events
-        div1.style.marginTop = "-490px";
-        setTimeout(function(){ div1.style.marginTop = "0px"; div1.style.display= "inline-block"; div1.style.transition = "0.3s";}, 300);
-    }
-    if(keyName === "2"){
-        //Pressed Events
-        div2.style.marginTop = "-490px";
-        setTimeout(function(){ div2.style.marginTop = "0px"; div2.style.display= "inline-block"; div2.style.transition = "0.3s";}, 300);
-    }
-    if(keyName === "3"){
-        //Pressed Events
-        div3.style.marginTop = "-490px";
-        setTimeout(function(){ div3.style.marginTop = "0px"; div3.style.display= "inline-block"; div3.style.transition = "0.3s";}, 300);
-    }
-    if(keyName === "4"){
-        //Pressed Events
-        div4.style.marginTop = "-490px";
-        setTimeout(function(){ div4.style.marginTop = "0px"; div4.style.display= "inline-block"; div4.style.transition = "0.3s";}, 300);
-    }
-    if(keyName === "5"){
-        //Pressed Events
-        div5.style.marginTop = "-490px";
-        setTimeout(function(){ div5.style.marginTop = "0px"; div5.style.display= "inline-block"; div5.style.transition = "0.3s";}, 300);
-    }
-    if(keyName === "6"){
-        //Pressed Events
-        div6.style.marginTop = "-490px";
-        setTimeout(function(){ div6.style.marginTop = "0px"; div6.style.display= "inline-block"; div6.style.transition = "0.3s";}, 300);
-    }
-    if(keyName === "7"){
-        //Pressed Events
-        div7.style.marginTop = "-490px";
-        setTimeout(function(){ div7.style.marginTop = "0px"; div7.style.display= "inline-block"; div7.style.transition = "0.3s";}, 300);
-    }
-    if(keyName === "8"){
-        //Pressed Events
-        div8.style.marginTop = "-490px";
-        setTimeout(function(){ div8.style.marginTop = "0px"; div8.style.display= "inline-block"; div8.style.transition = "0.3s";}, 300);
-    }
-    if(keyName === "9"){
-        //Pressed Events
-        div9.style.marginTop = "-490px";
-        setTimeout(function(){ div9.style.marginTop = "0px"; div9.style.display= "inline-block"; div9.style.transition = "0.3s";}, 300);
-    }
-    if(keyName === "0"){
-        //Pressed Events
-        div10.style.marginTop = "-490px";
-        setTimeout(function(){ div10.style.marginTop = "0px"; div10.style.display= "inline-block"; div10.style.transition = "0.3s";}, 300);
-    }
+    const div = document.getElementById(`box-${keyName}`);
+    div.style.marginTop = "-490px";
+    setTimeout(function(){ div.style.marginTop = "0px"; div.style.display= "inline-block"; div.style.transition = "0.3s";}, 300);
+
 })
